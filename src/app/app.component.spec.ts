82b02ea7945696f119380612ac5a79b4453a8ba8
Gean Guimarães Carneiro <gean.carneiro@sep.local>
2024-10-30 15:55:28 -0300

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'pip' title`, () => {
+  it(`should have the 'spo-web' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pip');
+    expect(app.title).toEqual('spo-web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pip');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, spo-web');
   });
 });
